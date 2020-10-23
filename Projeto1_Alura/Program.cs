a1cdd546af1969fa8970efc1b1903e233ca0ecdf
Yuri-Venicius <yvenicios@gmail.com>
2020-10-22 22:30:59 -0400

    Adicionar arquivos de projeto.

---
+++ Projeto1_Alura/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto1_Alura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Criando o projeto para teste do GitHub");
+            Console.ReadLine();
+        }
+    }
+}
